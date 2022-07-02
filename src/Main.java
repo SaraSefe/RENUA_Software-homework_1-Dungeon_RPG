1bc3713464c19b71cf3b6d115fb52f3ad2578c39
Joel Campos <joelcamposoliva9000@gmail.com>
2022-07-02 17:15:09 +0200

    Wizard can attack

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wizard wizard = new Wizard(1, "aaa", 100 ,true);
+        wizard.attack(wizard);
     }
 }
